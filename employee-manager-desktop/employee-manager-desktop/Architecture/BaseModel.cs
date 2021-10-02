5ca787562c35d96c129ffc18a19e2ef2ce0236f0
ArGoN <70173258+ArGoNArGoN@users.noreply.github.com>
2021-10-02 19:50:09 +0300

    add folder and base class

---
+++ employee-manager-desktop/employee-manager-desktop/Architecture/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_manager_desktop.Architecture
+{
+    internal abstract class BaseModel
+    {
+        public virtual Boolean IsValid { get; } = true;
+    }
+}
---
+++ employee-manager-desktop/employee-manager-desktop/MVVM/ViewModels/UserWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using employee_manager_desktop.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_manager_desktop.MVVM.ViewModels
+{
+    internal class UserWindowViewModel
+        : BaseViewModel
+    {
+        public UserWindowViewModel()
+            : base() { }
+    }
+}
